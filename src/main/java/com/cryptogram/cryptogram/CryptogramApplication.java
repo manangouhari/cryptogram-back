c191d9400fd7c8bb416a4890184feb91c6121faa
Manan Gouhari <gouhari21@hotmail.com>
2021-06-01 13:11:10 +0530

    Fetch and add NFTs periodically. Next, don't insert duplicate NFTs.

--- src/main/java/com/cryptogram/cryptogram/CryptogramApplication.java
+++ src/main/java/com/cryptogram/cryptogram/CryptogramApplication.java
@@ -1,26 +1,82 @@
 package com.cryptogram.cryptogram;
 
+import com.cryptogram.cryptogram.nft.NFT;
+import com.cryptogram.cryptogram.nft.NFTRepository;
+import com.cryptogram.cryptogram.nft.fetcher.NFTFetcher;
+import com.cryptogram.cryptogram.nft.fetcher.bazaar.BazaarModel;
+import com.cryptogram.cryptogram.nft.fetcher.hicetnunc.HicetnuncModel;
+import com.cryptogram.cryptogram.nft.fetcher.kalamint.KalamintModel;
+import com.cryptogram.cryptogram.nft.fetcher.mandala.MandalaModel;
+import com.squareup.moshi.Moshi;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 @SpringBootApplication()
 @EnableScheduling
 public class CryptogramApplication {
+	private final NFTRepository repository;
+
+	@Autowired
+	public CryptogramApplication(NFTRepository repo){
+		this.repository = repo;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptogramApplication.class, args);
 	}
 
-
-	@Scheduled(fixedDelay = 60 * 1000 * 5)
+	// Runs every 5 seconds
+	@Scheduled(fixedDelay = 1000 * 15)
 	public void someJob() throws IOException {
-		System.out.println(new Date());
+		try {
+			OkHttpClient client = new OkHttpClient();
+			Moshi moshi =  new Moshi.Builder().build();
+			NFTFetcher fetcher = new NFTFetcher(client, moshi);
+			List<NFT> nftsForDB = new ArrayList<>();
+
+			for(HicetnuncModel nft: fetcher.fetchHicetnuncTokens(0)){
+				nftsForDB.add(nft.convertToNFT());
+			}
+
+			HashMap<String, Boolean> nftTracker = new HashMap<String, Boolean>();
+			for (KalamintModel nft : fetcher.fetchKalamintTokens(0)) {
+				var kmNFT = nft.convertToNFT();
+
+				Boolean isNFTAlreadyAdded = nftTracker.getOrDefault("%s-%s".formatted(kmNFT.getName(), kmNFT.getResourceUrl()), false);
+				if (!isNFTAlreadyAdded) {
+					nftsForDB.add(kmNFT);
+					nftTracker.put("%s-%s".formatted(kmNFT.getName(), kmNFT.getResourceUrl()), true);
+				}
+			}
+			nftTracker.clear();
+
+			for (BazaarModel nft : fetcher.fetchBazaarTokens(0)) {
+				if(nft.getDisplay_uri() == null) continue;
+				nftsForDB.add(nft.convertToNFT());
+			}
+
+			for (MandalaModel nft : fetcher.fetchMandalaTokens(0)){
+				nftsForDB.add(nft.convertToNFT());
+			}
+
+			repository.saveAll(nftsForDB);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+
 	}
 
 
--- src/main/java/com/cryptogram/cryptogram/nft/NFTConfig.java
+++ src/main/java/com/cryptogram/cryptogram/nft/NFTConfig.java
@@ -1,20 +1,10 @@
 package com.cryptogram.cryptogram.nft;
 
-import com.cryptogram.cryptogram.nft.fetcher.NFTFetcher;
-import com.cryptogram.cryptogram.nft.fetcher.bazaar.BazaarModel;
-import com.cryptogram.cryptogram.nft.fetcher.hicetnunc.HicetnuncModel;
-import com.cryptogram.cryptogram.nft.fetcher.kalamint.KalamintModel;
-import com.cryptogram.cryptogram.nft.fetcher.mandala.MandalaModel;
-import com.squareup.moshi.Moshi;
-import okhttp3.OkHttpClient;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 @Configuration
 public class NFTConfig {
@@ -23,61 +13,61 @@ public class NFTConfig {
     CommandLineRunner commandLineRunner(NFTRepository repository) throws IOException {
 
         return args -> {
-            try{
-                System.out.println("Run on config");
-                OkHttpClient client = new OkHttpClient();
-                Moshi moshi =  new Moshi.Builder().build();
-
-                NFTFetcher fetcher = new NFTFetcher(client, moshi);
-
-                List<NFT> nftsForDB = new ArrayList<>();
-
-
-                for(HicetnuncModel nft: fetcher.fetchHicetnuncTokens(0)){
-//                    nftsForDB.add(
-//                            new NFT("Hicetnunc",
-//                                    "https://cloudflare-ipfs.com/ipfs/%s".formatted(nft.getArtifact_uri().substring(7)),
-//                                    nft.getToken_id(),
-//                                    nft.convertTimestampToDateTime(),
-//                                    nft.getUrlToPlatform(),
-//                                    nft.getName())
-//                    );
-                    nftsForDB.add(nft.convertToNFT());
-                }
-
-
-
-                HashMap<String, Boolean> nftTracker = new HashMap<String, Boolean>();
-
-                for (KalamintModel nft : fetcher.fetchKalamintTokens(0)) {
-                    var kmNFT = nft.convertToNFT();
-
-                    Boolean isNFTAlreadyAdded = nftTracker.getOrDefault("%s-%s".formatted(kmNFT.getName(), kmNFT.getResourceUrl()), false);
-                    if (!isNFTAlreadyAdded) {
-                        nftsForDB.add(kmNFT);
-                        nftTracker.put("%s-%s".formatted(kmNFT.getName(), kmNFT.getResourceUrl()), true);
-                    }
-                }
-
-                nftTracker.clear();
-
-
-                for (BazaarModel nft : fetcher.fetchBazaarTokens(0)) {
-                    if(nft.getDisplay_uri() == null) continue;
-                    nftsForDB.add(nft.convertToNFT());
-                }
-
-                for (MandalaModel nft : fetcher.fetchMandalaTokens(0)){
-                    nftsForDB.add(nft.convertToNFT());
-                }
-
-
-
-                repository.saveAll(nftsForDB);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+//            try{
+//                System.out.println("Run on config");
+//                OkHttpClient client = new OkHttpClient();
+//                Moshi moshi =  new Moshi.Builder().build();
+//
+//                NFTFetcher fetcher = new NFTFetcher(client, moshi);
+//
+//                List<NFT> nftsForDB = new ArrayList<>();
+//
+//
+//                for(HicetnuncModel nft: fetcher.fetchHicetnuncTokens(0)){
+////                    nftsForDB.add(
+////                            new NFT("Hicetnunc",
+////                                    "https://cloudflare-ipfs.com/ipfs/%s".formatted(nft.getArtifact_uri().substring(7)),
+////                                    nft.getToken_id(),
+////                                    nft.convertTimestampToDateTime(),
+////                                    nft.getUrlToPlatform(),
+////                                    nft.getName())
+////                    );
+//                    nftsForDB.add(nft.convertToNFT());
+//                }
+//
+//
+//
+//                HashMap<String, Boolean> nftTracker = new HashMap<String, Boolean>();
+//
+//                for (KalamintModel nft : fetcher.fetchKalamintTokens(0)) {
+//                    var kmNFT = nft.convertToNFT();
+//
+//                    Boolean isNFTAlreadyAdded = nftTracker.getOrDefault("%s-%s".formatted(kmNFT.getName(), kmNFT.getResourceUrl()), false);
+//                    if (!isNFTAlreadyAdded) {
+//                        nftsForDB.add(kmNFT);
+//                        nftTracker.put("%s-%s".formatted(kmNFT.getName(), kmNFT.getResourceUrl()), true);
+//                    }
+//                }
+//
+//                nftTracker.clear();
+//
+//
+//                for (BazaarModel nft : fetcher.fetchBazaarTokens(0)) {
+//                    if(nft.getDisplay_uri() == null) continue;
+//                    nftsForDB.add(nft.convertToNFT());
+//                }
+//
+//                for (MandalaModel nft : fetcher.fetchMandalaTokens(0)){
+//                    nftsForDB.add(nft.convertToNFT());
+//                }
+//
+//
+//
+////                repository.saveAll(nftsForDB);
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
 
         };
     }
